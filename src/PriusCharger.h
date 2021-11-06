f370f84342da6eeaba3231a633cd27fd2e04ccfe
Kevin McAndrew <kevinjmcandrew@gmail.com>
2021-11-06 20:58:34 +0000

    Added include guards and reference to Inverter.h in PriusCharger.h

---
+++ src/PriusCharger.h
@@ -0,0 +1,15 @@
+/* PriusCharger.h
+ * A header file for a controller designed to use the Toyota Prius Gen 2 (2004) inverter to
+ * charge batteries from an AC source.
+ *
+ * Date created: 06/11/2021
+ * Author: Kevin McAndrew
+ *
+*/
+
+#ifndef TRAPCONTROL_H
+#define TRAPCONTROL_H
+
+#include "inverter/Inverter.h"
+
+#endif
