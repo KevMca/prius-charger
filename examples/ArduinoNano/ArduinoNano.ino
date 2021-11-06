b9df19c6836d6aa0d45cc68286cb7434ce90fc2e
Kevin McAndrew <kevinjmcandrew@gmail.com>
2021-11-06 20:57:47 +0000

    Changed examples layout so that the example would be recognised in the Arduino IDE

---
+++ examples/ArduinoNano/ArduinoNano.ino
@@ -0,0 +1,18 @@
+/* prius-charger.ino
+ * A sketch designed to control the Toyota Prius Gen 2 (2004) inverter to
+ * charge batteries from an AC source.
+ *
+ * Date created: 06/11/2021
+ * Author: Kevin McAndrew
+ *
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
